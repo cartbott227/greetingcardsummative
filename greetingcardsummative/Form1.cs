bfd3c0a4aeb47c4b05196382dce6e4542a0514cc
temp <temp>
2016-09-19 12:43:16 -0400

    Created comment with name, date created and purpose.

--- greetingcardsummative/Form1.cs
+++ greetingcardsummative/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Created by Carter Bott
+// on September 19th, 2016
+// to complete the greeting card summative
+
 namespace greetingcardsummative
 {
     public partial class Form1 : Form
